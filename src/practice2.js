981feb77c87febe0f453748767efa136200b5160
yi-dai <yi.dai@thoughtworks.com>
2020-11-09 22:10:59 +0800

    solve question2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,9 @@
-const inject
+const inject = (items, sections) => {
+    let i = 0;
+    sections.forEach(section => {
+        items.splice(section.index + i, 0, section.content);
+        i ++;
+    })
+    return items;
+}
 export { inject };
