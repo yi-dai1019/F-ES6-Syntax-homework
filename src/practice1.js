5de58e9cb72c59fb6dfa094ec595e8e7c87f05b8
yi-dai <yi.dai@thoughtworks.com>
2020-11-09 22:10:44 +0800

    solve question1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,9 @@
-const parseData
+const parseData = ({ data, column }) => {
+    const keyList = column.map(element => element.name);
+    return data.map(element => {
+        const obj = {};
+        keyList.forEach((key, index) => obj[key] = element[index]);
+        return obj;
+    })
+}
 export { parseData };
